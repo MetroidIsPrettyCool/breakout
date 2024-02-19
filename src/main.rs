d6b1bd7afee5e683eb40f398f08345059bfccc26
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2024-02-19 01:03:38 -0800

    immutable vbos

--- src/main.rs
+++ src/main.rs
@@ -12,63 +12,68 @@ use winit::window::Window;
 
 /// Things that can be drawn to the screen
 pub trait Drawable {
-    fn draw(
-        &self,
-        frame: &mut Frame,
-        display: &Display<WindowSurface>,
-        program: &Program,
-        game_state: &WindowState,
-    ) -> Result<(), Box<dyn Error>> {
+    fn draw(&self, frame: &mut Frame, window_state: &WindowState) -> Result<(), Box<dyn Error>> {
         let uniforms = uniform! {
             offset: self.get_vertex_offset(),
-            window_aspect: [game_state.window_width, game_state.window_height],
+            window_aspect: [window_state.window.inner_size().width as f32, window_state.window.inner_size().height as f32],
         };
-        let vertices = VertexBuffer::new(display, &self.get_model())?;
         frame.draw(
-            &vertices,
+            self.get_model(),
             IndicesSource::NoIndices {
                 primitives: glium::index::PrimitiveType::TrianglesList,
             },
-            program,
+            &window_state.program,
             &uniforms,
             &DrawParameters::default(),
         )?;
         Ok(())
     }
 
-    fn get_model(&self) -> Vec<Vertex>;
+    fn get_model(&self) -> &VertexBuffer<Vertex>;
 
     fn get_vertex_offset(&self) -> [f32; 3];
 }
 
 /// Paddle
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Debug)]
 pub struct Paddle {
     /// Position of the center of the paddle
     pub x: f32,
+    /// vbo
+    pub vbo: VertexBuffer<Vertex>,
 }
 impl Paddle {
     pub const WIDTH: f32 = 0.25;
     pub const HEIGHT: f32 = 0.025;
     pub const COLOR: [f32; 3] = [0.0, 1.0, 0.5];
     pub const VERTICAL_OFFSET: f32 = -0.950;
+
+    pub const MODEL: [Vertex; 3] = [
+        Vertex {
+            position: [-Paddle::WIDTH / 2.0, Paddle::HEIGHT / 2.0, 0.0],
+            color: Paddle::COLOR,
+        },
+        Vertex {
+            position: [0.0, -Paddle::HEIGHT / 2.0, 0.0],
+            color: Paddle::COLOR,
+        },
+        Vertex {
+            position: [Paddle::WIDTH / 2.0, Paddle::HEIGHT / 2.0, 0.0],
+            color: Paddle::COLOR,
+        },
+    ];
+
+    fn new(x: f32, display: &Display<WindowSurface>) -> Paddle {
+        Paddle {
+            x,
+            vbo: VertexBuffer::immutable(display, &Self::MODEL)
+                .expect("unable to create paddle VBO, exiting"),
+        }
+    }
 }
 impl Drawable for Paddle {
-    fn get_model(&self) -> Vec<Vertex> {
-        vec![
-            Vertex {
-                position: [-Paddle::WIDTH / 2.0, Paddle::HEIGHT / 2.0, 0.0],
-                color: Paddle::COLOR,
-            },
-            Vertex {
-                position: [0.0, -Paddle::HEIGHT / 2.0, 0.0],
-                color: Paddle::COLOR,
-            },
-            Vertex {
-                position: [Paddle::WIDTH / 2.0, Paddle::HEIGHT / 2.0, 0.0],
-                color: Paddle::COLOR,
-            },
-        ]
+    fn get_model(&self) -> &VertexBuffer<Vertex> {
+        &self.vbo
     }
 
     fn get_vertex_offset(&self) -> [f32; 3] {
@@ -76,8 +81,8 @@ impl Drawable for Paddle {
     }
 }
 
-/// Paddle
-#[derive(Copy, Clone, Debug, PartialEq)]
+/// Ball
+#[derive(Debug)]
 pub struct Ball {
     /// Position of the center of the ball
     pub x: f32,
@@ -86,40 +91,56 @@ pub struct Ball {
     /// Velocity
     pub x_v: f32,
     pub y_v: f32,
+
+    /// vbo
+    pub vbo: VertexBuffer<Vertex>,
 }
 impl Ball {
     pub const WIDTH: f32 = 0.025;
     pub const HEIGHT: f32 = 0.025;
     pub const COLOR: [f32; 3] = [0.259, 0.051, 0.671];
+
+    pub const MODEL: [Vertex; 6] = [
+        Vertex {
+            position: [(Ball::WIDTH / 2.0), (Ball::HEIGHT / 2.0), 0.0],
+            color: Ball::COLOR,
+        },
+        Vertex {
+            position: [-(Ball::WIDTH / 2.0), (Ball::HEIGHT / 2.0), 0.0],
+            color: Ball::COLOR,
+        },
+        Vertex {
+            position: [-(Ball::WIDTH / 2.0), -(Ball::HEIGHT / 2.0), 0.0],
+            color: Ball::COLOR,
+        },
+        Vertex {
+            position: [(Ball::WIDTH / 2.0), (Ball::HEIGHT / 2.0), 0.0],
+            color: Ball::COLOR,
+        },
+        Vertex {
+            position: [-(Ball::WIDTH / 2.0), -(Ball::HEIGHT / 2.0), 0.0],
+            color: Ball::COLOR,
+        },
+        Vertex {
+            position: [(Ball::WIDTH / 2.0), -(Ball::HEIGHT / 2.0), 0.0],
+            color: Ball::COLOR,
+        },
+    ];
+
+    fn new(x: f32, y: f32, x_v: f32, y_v: f32, display: &Display<WindowSurface>) -> Ball {
+        Ball {
+            x,
+            y,
+            x_v,
+            y_v,
+            vbo: VertexBuffer::immutable(display, &Self::MODEL)
+                .expect("unable to create ball VBO, exiting"),
+        }
+    }
 }
 impl Drawable for Ball {
-    fn get_model(&self) -> Vec<Vertex> {
-        vec![
-            Vertex {
-                position: [(Ball::WIDTH / 2.0), (Ball::HEIGHT / 2.0), 0.0],
-                color: Ball::COLOR,
-            },
-            Vertex {
-                position: [-(Ball::WIDTH / 2.0), (Ball::HEIGHT / 2.0), 0.0],
-                color: Ball::COLOR,
-            },
-            Vertex {
-                position: [-(Ball::WIDTH / 2.0), -(Ball::HEIGHT / 2.0), 0.0],
-                color: Ball::COLOR,
-            },
-            Vertex {
-                position: [(Ball::WIDTH / 2.0), (Ball::HEIGHT / 2.0), 0.0],
-                color: Ball::COLOR,
-            },
-            Vertex {
-                position: [-(Ball::WIDTH / 2.0), -(Ball::HEIGHT / 2.0), 0.0],
-                color: Ball::COLOR,
-            },
-            Vertex {
-                position: [(Ball::WIDTH / 2.0), -(Ball::HEIGHT / 2.0), 0.0],
-                color: Ball::COLOR,
-            },
-        ]
+    fn get_model(&self) -> &VertexBuffer<Vertex> {
+        &self.vbo
     }
 
     fn get_vertex_offset(&self) -> [f32; 3] {
@@ -128,39 +149,51 @@ impl Drawable for Ball {
 }
 
 // Game playfield
-#[derive(Copy, Clone, Debug, PartialEq)]
-pub struct Playfield {}
+#[derive(Debug)]
+pub struct Playfield {
+    /// vbo
+    pub vbo: VertexBuffer<Vertex>,
+}
 impl Playfield {
     pub const COLOR: [f32; 3] = [1.0, 1.0, 1.0];
+
+    pub const MODEL: [Vertex; 6] = [
+        Vertex {
+            position: [1.0, 1.0, 0.0],
+            color: Playfield::COLOR,
+        },
+        Vertex {
+            position: [-1.0, 1.0, 0.0],
+            color: Playfield::COLOR,
+        },
+        Vertex {
+            position: [-1.0, -1.0, 0.0],
+            color: Playfield::COLOR,
+        },
+        Vertex {
+            position: [1.0, 1.0, 0.0],
+            color: Playfield::COLOR,
+        },
+        Vertex {
+            position: [-1.0, -1.0, 0.0],
+            color: Playfield::COLOR,
+        },
+        Vertex {
+            position: [1.0, -1.0, 0.0],
+            color: Playfield::COLOR,
+        },
+    ];
+
+    fn new(display: &Display<WindowSurface>) -> Playfield {
+        Playfield {
+            vbo: VertexBuffer::immutable(display, &Self::MODEL)
+                .expect("unable to create playfield VBO, exiting"),
+        }
+    }
 }
 impl Drawable for Playfield {
-    fn get_model(&self) -> Vec<Vertex> {
-        vec![
-            Vertex {
-                position: [1.0, 1.0, 0.0],
-                color: Playfield::COLOR,
-            },
-            Vertex {
-                position: [-1.0, 1.0, 0.0],
-                color: Playfield::COLOR,
-            },
-            Vertex {
-                position: [-1.0, -1.0, 0.0],
-                color: Playfield::COLOR,
-            },
-            Vertex {
-                position: [1.0, 1.0, 0.0],
-                color: Playfield::COLOR,
-            },
-            Vertex {
-                position: [-1.0, -1.0, 0.0],
-                color: Playfield::COLOR,
-            },
-            Vertex {
-                position: [1.0, -1.0, 0.0],
-                color: Playfield::COLOR,
-            },
-        ]
+    fn get_model(&self) -> &VertexBuffer<Vertex> {
+        &self.vbo
     }
 
     fn get_vertex_offset(&self) -> [f32; 3] {
@@ -169,11 +202,14 @@ impl Drawable for Playfield {
 }
 
 /// Brick
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Debug)]
 pub struct Brick {
     /// Position of the center of the brick
     pub x: f32,
     pub y: f32,
+
+    /// vbo
+    pub vbo: VertexBuffer<Vertex>,
 }
 impl Brick {
     pub const COLOR: [f32; 3] = [0.5, 0.0, 0.1];
@@ -182,35 +218,46 @@ impl Brick {
 
     pub const ROWS: usize = 20;
     pub const COLUMNS: usize = 40;
+
+    pub const MODEL: [Vertex; 6] = [
+        Vertex {
+            position: [(Brick::WIDTH / 2.0), (Brick::HEIGHT / 2.0), 0.0],
+            color: Brick::COLOR,
+        },
+        Vertex {
+            position: [-(Brick::WIDTH / 2.0), (Brick::HEIGHT / 2.0), 0.0],
+            color: Brick::COLOR,
+        },
+        Vertex {
+            position: [-(Brick::WIDTH / 2.0), -(Brick::HEIGHT / 2.0), 0.0],
+            color: Brick::COLOR,
+        },
+        Vertex {
+            position: [(Brick::WIDTH / 2.0), (Brick::HEIGHT / 2.0), 0.0],
+            color: Brick::COLOR,
+        },
+        Vertex {
+            position: [-(Brick::WIDTH / 2.0), -(Brick::HEIGHT / 2.0), 0.0],
+            color: Brick::COLOR,
+        },
+        Vertex {
+            position: [(Brick::WIDTH / 2.0), -(Brick::HEIGHT / 2.0), 0.0],
+            color: Brick::COLOR,
+        },
+    ];
+
+    pub fn new(x: f32, y: f32, display: &Display<WindowSurface>) -> Brick {
+        Brick {
+            x,
+            y,
+            vbo: VertexBuffer::immutable(display, &Self::MODEL)
+                .expect("unable to create brick VBO, exiting"),
+        }
+    }
 }
 impl Drawable for Brick {
-    fn get_model(&self) -> Vec<Vertex> {
-        vec![
-            Vertex {
-                position: [(Brick::WIDTH / 2.0), (Brick::HEIGHT / 2.0), 0.0],
-                color: Brick::COLOR,
-            },
-            Vertex {
-                position: [-(Brick::WIDTH / 2.0), (Brick::HEIGHT / 2.0), 0.0],
-                color: Brick::COLOR,
-            },
-            Vertex {
-                position: [-(Brick::WIDTH / 2.0), -(Brick::HEIGHT / 2.0), 0.0],
-                color: Brick::COLOR,
-            },
-            Vertex {
-                position: [(Brick::WIDTH / 2.0), (Brick::HEIGHT / 2.0), 0.0],
-                color: Brick::COLOR,
-            },
-            Vertex {
-                position: [-(Brick::WIDTH / 2.0), -(Brick::HEIGHT / 2.0), 0.0],
-                color: Brick::COLOR,
-            },
-            Vertex {
-                position: [(Brick::WIDTH / 2.0), -(Brick::HEIGHT / 2.0), 0.0],
-                color: Brick::COLOR,
-            },
-        ]
+    fn get_model(&self) -> &VertexBuffer<Vertex> {
+        &self.vbo
     }
 
     fn get_vertex_offset(&self) -> [f32; 3] {
@@ -219,7 +266,7 @@ impl Drawable for Brick {
 }
 
 /// Information relevant to the renderer
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Debug)]
 pub struct WindowState {
     pub frame_count: u64,
     pub then: Instant,
@@ -231,9 +278,22 @@ pub struct WindowState {
 
     pub window_width: f32,
     pub window_height: f32,
+
+    pub program: Program,
+    pub window: Window,
+    pub display: Display<WindowSurface>,
 }
 impl WindowState {
-    pub fn new(window: &Window) -> WindowState {
+    pub fn new(window: Window, display: Display<WindowSurface>) -> WindowState {
+        // set up shaders
+        let program = glium::Program::from_source(
+            &display,
+            include_str!("vert.glsl"),
+            include_str!("frag.glsl"),
+            None,
+        )
+        .expect("unable to compile shaders, exiting");
+
         let window_size = window.inner_size();
         WindowState {
             frame_count: 0,
@@ -245,6 +305,10 @@ impl WindowState {
 
             window_width: (window_size.width as f32),
             window_height: (window_size.height as f32),
+
+            program,
+            window,
+            display,
         }
     }
 }
@@ -267,24 +331,10 @@ fn main() {
     // set up window
     let (window, display) = SimpleWindowBuilder::new().build(&event_loop);
 
-    // set up shaders
-    let program = glium::Program::from_source(
-        &display,
-        include_str!("vert.glsl"),
-        include_str!("frag.glsl"),
-        None,
-    )
-    .expect("unable to compile shaders, exiting");
-
     // TMP
-    let mut paddle = Paddle { x: 0.0 };
-    let playfield = Playfield {};
-    let mut ball = Ball {
-        x: 0.0,
-        y: 0.0,
-        x_v: 0.5,
-        y_v: -0.86602540378,
-    };
+    let mut paddle = Paddle::new(0.0, &display);
+    let playfield = Playfield::new(&display);
+    let mut ball = Ball::new(0.0, 0.0, 0.5, -0.86602540378, &display);
     let mut bricks: Vec<Vec<Brick>> = Vec::new();
     for i in 0..Brick::ROWS {
         bricks.push(Vec::new());
@@ -292,11 +342,11 @@ fn main() {
         for j in 0..Brick::COLUMNS {
             // y coords are top half only
             let y = j as f32 * 1.0 / Brick::COLUMNS as f32;
-            bricks[i].push(Brick { x, y });
+            bricks[i].push(Brick::new(x, y, &display));
         }
     }
 
-    let mut game_state = WindowState::new(&window);
+    let mut window_state = WindowState::new(window, display);
 
     event_loop
         .run(move |event, window_target| match event {
@@ -304,12 +354,14 @@ fn main() {
                 event: WindowEvent::CloseRequested,
                 ..
             } => {
-                let time_elapsed = Instant::now().duration_since(game_state.then).as_secs_f32();
-                println!("frame_count: {}", game_state.frame_count);
+                let time_elapsed = Instant::now()
+                    .duration_since(window_state.then)
+                    .as_secs_f32();
+                println!("frame_count: {}", window_state.frame_count);
                 println!("time_elapsed: {} secs", time_elapsed);
                 println!(
                     "frames per second: {}",
-                    game_state.frame_count as f32 / time_elapsed
+                    window_state.frame_count as f32 / time_elapsed
                 );
 
                 window_target.exit();
@@ -321,37 +373,30 @@ fn main() {
                 let PhysicalSize {
                     width: window_width,
                     height: window_height,
-                } = window.inner_size();
+                } = window_state.window.inner_size();
 
-                game_state.mouse_x_relative =
-                    (p.x as f32 / (window.inner_size().width / 2) as f32) - 1.0;
-                game_state.mouse_y_relative =
-                    (p.y as f32 / (window.inner_size().height / 2) as f32) - 1.0;
+                window_state.mouse_x_relative =
+                    (p.x as f32 / (window_state.window.inner_size().width / 2) as f32) - 1.0;
+                window_state.mouse_y_relative =
+                    (p.y as f32 / (window_state.window.inner_size().height / 2) as f32) - 1.0;
 
                 // correct for aspect ratio
                 if window_width > window_height {
-                    game_state.mouse_x_relative *= window_width as f32 / window_height as f32;
+                    window_state.mouse_x_relative *= window_width as f32 / window_height as f32;
                 } else {
-                    game_state.mouse_y_relative *= window_height as f32 / window_width as f32;
+                    window_state.mouse_y_relative *= window_height as f32 / window_width as f32;
                 }
-            }
-            Event::WindowEvent {
-                event: WindowEvent::Resized(s),
-                ..
-            } => {
-                game_state.window_width = s.width as f32;
-                game_state.window_height = s.height as f32;
             }
             Event::AboutToWait => {
                 // timey-wimey
                 let now = Instant::now();
-                let delta_t = match game_state.last_frame_was {
+                let delta_t = match window_state.last_frame_was {
                     Some(then) => now.duration_since(then),
                     None => Duration::ZERO,
                 };
 
                 // move paddle to mouse
-                paddle.x = (game_state.mouse_x_relative)
+                paddle.x = (window_state.mouse_x_relative)
                     .clamp(-1.0 + (Paddle::WIDTH / 2.0), 1.0 - (Paddle::WIDTH / 2.0));
 
                 // move ball
@@ -391,29 +436,29 @@ fn main() {
                 }
 
                 // draw a frame
-                let mut frame = display.draw();
+                let mut frame = window_state.display.draw();
 
                 frame.clear(None, Some((0.0, 0.0, 0.0, 1.0)), false, None, None);
 
                 // draw playfield
                 playfield
-                    .draw(&mut frame, &display, &program, &game_state)
+                    .draw(&mut frame, &window_state)
                     .expect("unable to draw playfield to frame, exiting");
 
                 // draw ball
-                ball.draw(&mut frame, &display, &program, &game_state)
+                ball.draw(&mut frame, &window_state)
                     .expect("unable to draw ball to frame, exiting");
 
                 // draw paddle
                 paddle
-                    .draw(&mut frame, &display, &program, &game_state)
+                    .draw(&mut frame, &window_state)
                     .expect("unable to draw paddle to frame, exiting");
 
                 // draw bricks
                 for column in bricks.iter() {
                     for brick in column {
                         brick
-                            .draw(&mut frame, &display, &program, &game_state)
+                            .draw(&mut frame, &window_state)
                             .expect("unable to draw brick to frame, exiting");
                     }
                 }
@@ -421,8 +466,8 @@ fn main() {
                 // wrap up
                 frame.finish().expect("unable to finish frame, exiting");
 
-                game_state.frame_count += 1;
-                game_state.last_frame_was = Some(now);
+                window_state.frame_count += 1;
+                window_state.last_frame_was = Some(now);
             }
             _ => (),
         })
